a5bc99f81140c4bbbdae19d2ba8bc475b4246b06
Ricardo Giantomaso <ricardo_tech@live.com>
2022-03-08 18:50:10 -0300

    Feat: aula 1 Aula 1 de exercícios do python

---
+++ aula1.py
@@ -0,0 +1,8 @@
+def soma3(a, b, c):
+    return a + b + c
+
+
+a = int(input('Digite um numero: '))
+b = int(input('Digite outro numero: '))
+c = int(input('Digite mais um caralho de numero: '))
+print(soma3(a, b, c))
